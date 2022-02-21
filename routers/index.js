32078bb2c73edbd7955956c10297a421b81127c4
bbb4756 <rlarbfl4756@gmail.com>
2022-02-21 18:02:10 +0900

    1일차입니다.

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const adminRouter=require('./admin')
 const router = express.Router()
 const mainRouter = require('./main')
+const userRouter = require('./user')
+// const boardRouter = require('./board')
+// const adminRouter = require('./admin')
 
-router.use('/',mainRouter)
+router.use('/',mainRouter, (req, res)=>{
+    res.send('ㅎㅇ')
+})
+router.use('/user',userRouter)
+// router.use('/board',boardRouter)
+// router.use('/admin',adminRouter)
 
 router.use('/admin',adminRouter)
 
