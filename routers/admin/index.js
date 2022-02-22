80537bc2097d95ee86296e05a77df856074d20a8
ko-yerin <rhdpfls12@naver.com>
2022-02-22 17:49:04 +0900

    2-22커밋

--- routers/admin/index.js
+++ routers/admin/index.js
@@ -1,4 +1,38 @@
 const express=require('express')
-const router=express.Router
+const app=express()
+const router=express.Router()
+
+app.use(express.urlencoded({extended:true,}))
+
+router.post('/login',(req,res)=>{
+    console.log('로그인완료')
+    res.send('로그인')
+})
+
+
+app.get('/admin/list/forced out',(req,res)=>{
+    res.render('')
+})
+
+
+app.post('/admin/list/forced out',(req,res)=>{
+    res.render('')
+})
+
+app.get('/admin/list/Managing_board',(req,res)=>{
+    res.render('')
+})
+
+app.post('/admin/list/Managing_board',(req,res)=>{
+    res.render('')
+})
+
+app.get('/admin/list/level adjustment',(req,res)=>{
+    res.render('')
+})
+
+app.post('/admin/list/level adjustment',(req,res)=>{
+    res.render('')
+})
 
 module.exports=router
