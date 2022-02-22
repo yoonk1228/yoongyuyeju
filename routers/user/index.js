e77eebfd813f256b4289251a5c0fb031aa80ee9c
yoonk1228 <yoonk1228@naver.com>
2022-02-22 18:35:19 +0900

    merge사항 수정

--- routers/user/index.js
+++ routers/user/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const loginController = require('./login.controller')
 // const profileController = require('./profile.controller')
-const signupController = require('./signup.controller')
+// todo signupController 규리님이 23일에 작성해서 올려주실것임
+// const signupController = require('./signup.controller')
 // const updateController = require('./update.controller')
 const db = require('../../db')
 
 router.get('/login',loginController.login)
-<<<<<<< HEAD
 // router.post('/login',loginController.loginAction)
 
 router.post('/logout', (req, res)=>{
@@ -18,10 +18,10 @@ router.get('/profile', (req,res)=>{
     res.render('user/profile')
 })
 
-
-router.get('/signup', signupController.join)
-
-router.post('/signup', signupController.joinAction)
+// todo 규리님이 23일에 작업
+// router.get('/signup', signupController.join)
+//
+// router.post('/signup', signupController.joinAction)
 
 router.get('/update', (req,res)=>{
     res.render('user/infoupdate')
@@ -35,8 +35,5 @@ router.post('/resign', (req,res)=>{
     res.send('회원 탈퇴 완료')
 })
 // router.get('/logout', loginController)
-=======
-// router.post('/login',login.Contoller.loginAction)
->>>>>>> 652c0388b5d66a7a8838d0ce0f3f9847ae33cc13
 
 module.exports = router
--- routers/user/login.controller.js
+++ routers/user/login.controller.js
@@ -1,24 +1,4 @@
 exports.login = (req,res) => {
-<<<<<<< HEAD
-    res.render('user/login')
-}
 
-// exports.loginAction = (req,res)=>{
-//     let {userid,userpw} = req.body
-//     let [item] = user.filter(v=>(v.userid == userid && v.userpw == userpw))
-//     //위 코드의 목적:
-//     //사용자에게 받은정보를 서버에있는 리스트중에서 하나만 가져오기위해서.
-//     //filter() = forEach()
-//     if(item != undefined){
-//         //로그인 할수있는경우
-//         req.session.user={ ...item }
-//         res.redirect('/')
-//     }else{
-//         //로그인 못하는경우
-//         res.send(alertmove('/user/login','아이디와 패스워드가 일치하지않습니다'))
-//     }
-// }
-=======
     res.render(`user/login`)
 }
->>>>>>> 652c0388b5d66a7a8838d0ce0f3f9847ae33cc13
