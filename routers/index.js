00dcc729aca8859b1fb54f451f2310d1f3756b05
yoonk1228 <yoonk1228@naver.com>
2022-02-22 12:18:10 +0900

    main페이지 라우터 등록, 페í페이지 연결

--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const adminRouter=require('./admin')
+// const mainRouter=require('./main')
 const router = express.Router()
 // const mainRouter = require('./main')
 
 // const userRouter = require('./user')
 const boardRouter = require('./board')
+const mainController = require("./main.controller");
 // const adminRouter = require('./admin')
 
 // router.use('/',mainRouter)
@@ -13,5 +15,7 @@ router.use('/board',boardRouter)
 // router.use('/admin',adminRouter)
 
 router.use('/admin',adminRouter)
+// router.use('/main',mainRouter)
+router.get('/',mainController.main)
 
 module.exports = router
--- routers/main/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const mainController = require('./main.controller')
-
-router.get('/',mainController.main)
-
-module.exports = router
