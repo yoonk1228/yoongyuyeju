5f9acbaa407e771663b5a5a28e85d335daa3d3e8
seungju <seungju121@naver.com>
2022-02-22 17:15:06 +0900

    2022-02-22 승주 오후수정

--- routers/board/index.js
+++ routers/board/index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const listRouter = require('./list/index.js')
+// const data = require('../../moels/뭐 어디지?')
 
 router.get('/list',(req,res)=>{
-    res.send('list 페이지 ㄱㄱ')
+    res.render('board/list.html')
 })
 
-router.use('/list',listRouter)
+// router.get('/list',(req,res)=>{
+//     res.render('board/list.html',{
+//         data,
+//     })
+// })
+    // 데이터베이스 오면 바로진행
+    // DB에서 가져온 데이터 넌젹스로 나열하기
+    // 만약 write에서 글이 써진다면 배열로 만들어진 DB라서 push를 해주면 자동으로 올라감
+
+router.use('/list',listRouter) //
 
 
 module.exports = router
